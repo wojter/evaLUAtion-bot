a9413a21d7ee9e1f84286586d823bdf621c865e7
Wojter <wojtektro2@gmail.com>
2022-12-05 17:30:09 +0100

    Add movement to trigger

--- oneline2.lua
+++ oneline2.lua
@@ -1,55 +1,93 @@
 maxHealth = 10
+botsSmallestDist = 99999
 function oneline2whatTo(agent, actorKnowledge, time)
     friends = actorKnowledge:getSeenFriends()
     enemies = actorKnowledge:getSeenFoes()
+    nav = actorKnowledge:getNavigation()
 
-    -- if (friends:size() > 0) then
-    --     direction = friends:at(0):getPosition() - actorKnowledge:getPosition()
-    --     distance = direction:length()
-    --     if (distance > 40) then
-    --         agent:moveDirection(direction)
-    --         agent:rotate(direction)
-    --     end
-    --     if (distance < 20) then
-    --         agent:wait()
-    --     end
-    -- end
-    -- if (actorKnowledge:getAmmo(Enumerations.RocketLuncher) ~= 0) then
-    --     agent:selectWeapon(Enumerations.RocketLuncher);
-    -- elseif (actorKnowledge:getAmmo(Enumerations.Railgun) ~= 0) then
-    --     agent:selectWeapon(Enumerations.Railgun);
-    -- elseif (actorKnowledge:getAmmo(Enumerations.Shotgun) ~= 0) then
-    --     agent:selectWeapon(Enumerations.Shotgun);
-    -- elseif (actorKnowledge:getAmmo(Enumerations.Chaingun) ~= 0) then
-    --     agent:selectWeapon(Enumerations.Chaingun);
-    -- end
-
-    canShoot = false
-    if (enemies:size() > 0) then
-        enemyDir = enemies:at(0):getPosition() - actorKnowledge:getPosition()
-        enemyDist = enemyDir:length()
-        if (friends:size() > 0) then
-            canShoot2 = true
-            for i = 0, friends:size() - 1, 1 do
-                friendEnemyDir = enemies:at(i):getPosition() - friends:at(i):getPosition()
-
-                friendlyFire = (enemyDir:value(0) * friendEnemyDir:value(0) + enemyDir:value(1) *
-                                   friendEnemyDir:value(1)) / (enemyDir:length() * friendEnemyDir:length())
-                io.write("test1\n", friendlyFire)
-                if (friendlyFire > 0.997 and friendEnemyDir:length() < dir:length()) then
-                    canShoot2 = false
-                    -- dir2 = Vector4d(-dir:value(1), dir:value(0), 0, 0)
-                    -- agent:moveDirection(dir2)
-
-                else
-                    canShoot2 = true
-                    -- agent:shootAtPoint(enemies:at(0):getPosition())
+    triggerToMove = -1
+    if (actorKnowledge:isMoving() == false) then
+        for i = 0, nav:getNumberOfTriggers() - 1, 1 do
+            trig = nav:getTrigger(i)
+            dist = trig:getPosition() - actorKnowledge:getPosition()
+            if (dist:length() < 150 and trig:isActive()) then
+                agent:moveTo(trig:getPosition())
+            end
+        end
+    end
+    if (actorKnowledge:getHealth() < maxHealth / 2) then
+        triggToMove = -1
+        triggToMoveDist = 99999
+        for i = 0, nav:getNumberOfTriggers() - 1, 1 do
+            trig = nav:getTrigger(i)
+            if (trig:isActive()) then
+                if (trig:getType() == Trigger.Health or trig:getType() == Trigger.Armour) then
+                    tempDist2 = trig:getPosition() - actorKnowledge:getPosition()
+                    tempDist = tempDist2:length()
+                    if (tempDist < triggToMoveDist) then
+                        triggToMoveDist = tempDist
+                        triggToMove = i
+                    end
                 end
             end
-            canShoot = canShoot2
-        else
-            canShoot = true
-            -- agent:shootAtPoint(enemies:at(0):getPosition())
+        end
+        if (triggToMove ~= -1) then
+            agent:moveTo(nav:getTrigger(triggToMove):getPosition())
+        end
+    end
+
+    if (actorKnowledge:getAmmo(Enumerations.RocketLuncher) == 0 and actorKnowledge:getAmmo(Enumerations.Railgun) == 0 and
+        actorKnowledge:getAmmo(Enumerations.Shotgun) == 0 and actorKnowledge:getAmmo(Enumerations.Chaingun) == 0) then
+        triggToMove = -1
+        triggToMoveDist = 9999
+        for i = 0, nav:getNumberOfTriggers() - 1, 1 do
+            trig = nav:getTrigger(i)
+
+            if (trig:getType() == Trigger.Weapon) then
+                tempDist = (trig:getPosition() - actorKnowledge:getPosition()):length()
+                if (tempDist < triggToMoveDist) then
+                    triggToMoveDist = tempDist
+                    triggToMove = i
+                end
+            end
+
+        end
+        if (triggToMove ~= -1) then
+            agent:moveTo(nav:getTrigger(triggToMove):getPosition())
+        end
+    end
+
+    whichShoot = -1
+    whichShootDist = 9999
+    if (enemies:size() > 0) then
+        for j = 0, enemies:size() - 1, 1 do
+            enemyDir = enemies:at(j):getPosition() - actorKnowledge:getPosition()
+            enemyDist = enemyDir:length()
+
+            canShoot2 = true
+            if (friends:size() > 0) then
+                for i = 0, friends:size() - 1, 1 do
+                    friendEnemyDir = enemies:at(j):getPosition() - friends:at(i):getPosition()
+                    friendlyFire = (enemyDir:value(0) * friendEnemyDir:value(0) + enemyDir:value(1) *
+                                       friendEnemyDir:value(1)) / (enemyDir:length() * friendEnemyDir:length())
+                    if (friendlyFire > 0.997 and friendEnemyDir:length() < enemyDir:length()) then
+                        canShoot2 = false
+                    end
+                end
+            end
+            if (canShoot2 == true) then
+                whichShoot = j
+                whichShootDist = enemyDist
+                if (whichShoot ~= -1) then
+                    if (enemyDist < whichShootDist - 30) then
+                        whichShoot = j
+                        whichShootDist = enemyDist
+                    end
+                else
+                    whichShoot = j
+                    whichShootDist = enemyDist
+                end
+            end
         end
     end
     if (actorKnowledge:getAmmo(actorKnowledge:getWeaponType()) == 0) then
@@ -63,99 +101,44 @@ function oneline2whatTo(agent, actorKnowledge, time)
         elseif (actorKnowledge:getAmmo(Enumerations.Chaingun) ~= 0) then
             agent:selectWeapon(Enumerations.Chaingun);
         end
-        -- agent:selectWeapon(2)
         io.write(actorKnowledge:getWeaponType(), "weapon\n")
-    elseif (canShoot == true) then
-        io.write(" weapon", actorKnowledge:getWeaponType())
-        io.write(" ammo", actorKnowledge:getAmmo(actorKnowledge:getWeaponType()))
-        agent:shootAt(enemies:at(0))
-        io.write("Agentshoot, ammoleft", actorKnowledge:getAmmo(actorKnowledge:getWeaponType()))
+    elseif (whichShoot ~= -1) then
+        -- dir = enemies:at(whichShoot):getPosition() - actorKnowledge:getPosition()
+        if (whichShootDist < 140) then
+            -- if (enemies:at(whichShoot):getHealth() > actorKnowledge:getHealth() and actorKnowledge:getHealth() <
+            --     maxHealth / 2) then
+            --     agent:moveDirection(dir * (-1))
+            if (actorKnowledge:getWeaponType() ~= Enumerations.Shotgun and actorKnowledge:getAmmo(Enumerations.Shotgun) >
+                0) then
+                agent:selectWeapon(Enumerations.Shotgun)
+            else
+                agent:shootAt(enemies:at(whichShoot))
+            end
+            -- end
+        elseif (whichShootDist < 350) then
+            if (actorKnowledge:getWeaponType() ~= Enumerations.Chaingun and
+                actorKnowledge:getAmmo(Enumerations.Chaingun) > 0) then
+                agent:selectWeapon(Enumerations.Chaingun)
+            else
+                agent:shootAt(enemies:at(whichShoot))
+            end
+        else
+            if (actorKnowledge:getWeaponType() ~= Enumerations.Railgun and actorKnowledge:getAmmo(Enumerations.Railgun) >
+                0) then
+                agent:selectWeapon(Enumerations.Railgun)
+            else
+                agent:shootAt(enemies:at(whichShoot))
+            end
+        end
 
-        -- agent:shootAt(enemies:at(0))
     end
-    -- io.write("ammo", actorKnowledge:getAmmo(Enumerations.Shotgun), "\n")
-
-    -- if (enemies:size() > 0) then
-    --     -- agent:moveTo(enemies:at(0):getPosition())
-    --     distance = (enemies:at(0):getPosition() - actorKnowledge:getPosition()):length()
-    --     if (distance < 200) then
-    --         if (actorKnowledge:getWeaponType() ~= Enumerations.Shotgun and actorKnowledge:getAmmo(Enumerations.Shotgun) >
-    --             0) then
-    --             agent:selectWeapon(Enumerations.Shotgun)
-    --         else
-    --             agent:shootAt(enemies:at(0))
-    --         end
-    --     elseif (distance < 500) then
-    --         if (actorKnowledge:getWeaponType() ~= Enumerations.Chaingun and
-    --             actorKnowledge:getAmmo(Enumerations.Chaingun) > 0) then
-    --             agent:selectWeapon(Enumerations.Chaingun)
-    --         else
-    --             agent:shootAt(enemies:at(0))
-    --         end
-    --     else
-    --         if (actorKnowledge:getWeaponType() ~= Enumerations.Railgun and actorKnowledge:getAmmo(Enumerations.Railgun) >
-    --             0) then
-    --             agent:selectWeapon(Enumerations.Railgun)
-    --         else
-    --             agent:shootAt(enemies:at(0))
-    --         end
-    --     end
-    -- end
-
-    -- addew = enemies:at(1):getPosition()
-    -- showVector(enemies:at(1):getPosition())
-    -- weapon = actorKnowledge:getWeaponType()
-    -- io.write("\n weapon", weapon)
-    -- weapon = weapon + 1
-    io.write(" enemies", enemies:size(), "\n")
-    if enemies:size() > 0 then
-        -- io.write(enemies:size())
-        -- agent:shootAt(addew)
-    end
-    -- agent:selectWeapon(weapon)
 
 end
 
 function oneline2onStart(agent, actorKnowledge, time)
-    -- dir = Vector4d(10,3,1,0)
-    -- showVector(dir:normal())
-    -- showVector(dir:normalize())
     if (maxHealth < actorKnowledge:getHealth()) then
         maxHealth = actorKnowledge:getHealth()
     end
-    -- agent:moveTo(actor2destinationdirection)
+    agent:selectWeapon(1)
 end
 
-function showVector(vector)
-    io.write("(")
-    io.write(vector:value(0))
-    io.write(",")
-    io.write(vector:value(1))
-    io.write(",")
-    io.write(vector:value(2))
-    io.write(",")
-    io.write(vector:value(3))
-    io.write(");")
-end
-
-function shootOpponent()
-end
-function chooseWeapon(agent, actorKnowledge, dist)
-    -- if (dist < 200) then
-
-    -- end
-    -- if (actorKnowledge:getAmmo(Enumerations.RocketLuncher) ~= 0) then
-    --     io.write("rocket")
-    --     agent:selectWeapon(Enumerations.RocketLuncher);
-    -- elseif (actorKnowledge:getAmmo(Enumerations.Railgun) ~= 0) then
-    --     io.write("rail")
-    --     agent:selectWeapon(Enumerations.Railgun);
-    -- elseif (actorKnowledge:getAmmo(Enumerations.Shotgun) ~= 0) then
-    --     io.write("shot")
-    --     agent:selectWeapon(Enumerations.Shotgun);
-    -- elseif (actorKnowledge:getAmmo(Enumerations.Chaingun) ~= 0) then
-    --     io.write("chain")
-    --     agent:selectWeapon(Enumerations.Chaingun);
-    -- end
-    -- return "cokolwiek"
-end
